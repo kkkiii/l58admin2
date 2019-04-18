8a190a443aee007db5808f62d0d2f543757dec53
liuhongshuo <2624886248@qq.com>
2019-04-18 16:39:46 +0800

    u

--- resources/views/admin/priviledge/edit_role.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 7
- * Date: 2019/4/12
- * Time: 11:52
- */
---
+++ routes/common.php
@@ -0,0 +1,9 @@
+<?php
+
+
+Route::group(['prefix' => 'common' , 'namespace'=>'Common'],function () {
+    Route::get('/area.get_cities/{id}', 'AreaController@getCities')->name('area.get_cities');
+    Route::get('/area.get_district/{id}', 'AreaController@getDistrict')->name('area.get_district');
+
+
+});
